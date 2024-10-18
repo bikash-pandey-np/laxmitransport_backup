b700aa49e8c8802826e980556e626c201f481f31
bikash-pandey-np <bikashaya@gmail.com>
2024-10-18 13:57:17 +0545

    Test FOr subdomain

--- routes/shipper.php
+++ routes/shipper.php
@@ -1,4 +1,5 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Shipper\AuthController;
 use App\Http\Controllers\Shipper\LocationController;
@@ -6,60 +7,52 @@ use App\Http\Controllers\Shipper\DashboardController;
 use App\Http\Controllers\Shipper\QuoteController;
 use Inertia\Inertia;
 
-
-Route::prefix('register')->group(function(){
-    Route::get('/', [AuthController::class, 'register'])
-        ->name('shipper.auth.register')->middleware('shipper_already_logged_in');
-    Route::post('/', [AuthController::class, 'store']);
-});
-
-Route::prefix('login')->group(function(){
-    Route::get('/', [AuthController::class, 'login'])
-        ->name('shipper.auth.login')->middleware('shipper_already_logged_in');
-    Route::post('/', [AuthController::class, 'handleLogin']);
-});
-
-Route::prefix('location')->group(function(){
-    Route::get('/states', [LocationController::class, 'getStates']);
-    Route::get('/districts', [LocationController::class, 'getDistricts']);
-    Route::get('/local-bodies', [LocationController::class, 'getLocalBodies']);
-});
-
-Route::middleware(['onlyShipper'])->group(function(){
-
-    Route::prefix('verify-email')->group(function(){
-    Route::get('/', [AuthController::class, 'getVerifyPage'])
-        ->name('shipper.auth.verify');
-        Route::post('/send-email', [AuthController::class, 'sendEmail']);
+Route::domain('shipper.laxmicloud.com')->group(function () {
+    Route::prefix('register')->group(function () {
+        Route::get('/', [AuthController::class, 'register'])
+            ->name('shipper.auth.register')
+            ->middleware('shipper_already_logged_in');
+        Route::post('/', [AuthController::class, 'store']);
     });
 
-    Route::post('/verify', [AuthController::class, 'verifyEmail']);
-
-  
-    
-});
-
-//protected routes
-Route::middleware(['onlyShipper'])->group(function(){
-
-    Route::get('/', function(){
-        return redirect()->route('shipper.dashboard');
+    Route::prefix('login')->group(function () {
+        Route::get('/', [AuthController::class, 'login'])
+            ->name('shipper.auth.login')
+            ->middleware('shipper_already_logged_in');
+        Route::post('/', [AuthController::class, 'handleLogin']);
     });
 
+    Route::prefix('location')->group(function () {
+        Route::get('/states', [LocationController::class, 'getStates']);
+        Route::get('/districts', [LocationController::class, 'getDistricts']);
+        Route::get('/local-bodies', [LocationController::class, 'getLocalBodies']);
+    });
 
+    Route::middleware(['onlyShipper'])->group(function () {
+        Route::prefix('verify-email')->group(function () {
+            Route::get('/', [AuthController::class, 'getVerifyPage'])
+                ->name('shipper.auth.verify');
+            Route::post('/send-email', [AuthController::class, 'sendEmail']);
+        });
 
-    Route::post('/logout', [AuthController::class, 'logout'])
-        ->name('shipper.logout');
+        Route::post('/verify', [AuthController::class, 'verifyEmail']);
+    });
 
-    Route::get('/dashboard', [DashboardController::class, 'getDashboard'])
-    ->name('shipper.dashboard');
-    
-    Route::get('/quote', [QuoteController::class, 'getQuotePage'])
-        ->name('shipper.quote');
+    // Protected routes
+    Route::middleware(['onlyShipper'])->group(function () {
+        Route::get('/', function () {
+            return redirect()->route('shipper.dashboard');
+        });
 
-    Route::post('/quote', [QuoteController::class, 'getQuoteForParcelandLtl']);
-    
+        Route::post('/logout', [AuthController::class, 'logout'])
+            ->name('shipper.logout');
 
+        Route::get('/dashboard', [DashboardController::class, 'getDashboard'])
+            ->name('shipper.dashboard');
+
+        Route::get('/quote', [QuoteController::class, 'getQuotePage'])
+            ->name('shipper.quote');
+
+        Route::post('/quote', [QuoteController::class, 'getQuoteForParcelandLtl']);
+    });
 });
-
-
